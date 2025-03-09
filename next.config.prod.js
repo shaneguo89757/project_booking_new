772388684d6a41fa8c3b74e633e7da080f065ff0
shaneguo89757 <shane89757@outlookc.com>
2025-03-09 18:15:11 +0800

    123

---
+++ next.config.prod.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/project_booking_new',
+}
+
+module.exports = nextConfig
